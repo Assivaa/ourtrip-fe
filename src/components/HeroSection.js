b7e9a136613e1d4c40bbb40c30499c55b9a52661
asiva syhrm <assiva.syahrani9e@gmail.com>
2022-12-13 23:49:14 +0700

    first commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
+      <h1> OurTrip </h1>
+      <p>Let's explore the world</p>
+      <div className='hero-btns'>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css'
+import HeroSection from '../HeroSection'
+
+function Home() {
+  console.log('Home')
+       return(
+         <>
+              <HeroSection/>
+         </>
+       )
+}
+
+export default Home;
