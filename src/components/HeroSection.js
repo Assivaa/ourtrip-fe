3504b5f256b6d7be94bbfb033ca24cab759f5f02
asiva syhrm <assiva.syahrani9e@gmail.com>
2022-12-15 23:23:42 +0700

    terbaru

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,12 +9,12 @@ function HeroSection() {
 
   return (
     <div className='hero-container'>
-      {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1> OurTrip </h1>
       <p>Let's explore the world</p>
       <div className='hero-btns'>
       {/* Link to component Cards */}
-        <Button className='btns' variant="outline-light" size="lg" href="#content">
+        <Button variant="outline-light">
           Get Started
         </Button>
       </div>
