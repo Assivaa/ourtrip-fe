058a9b3c7526cc2c3e99b3f41a1140d61e4ad02d
Assiva Syahrani <111671065+Assivaa@users.noreply.github.com>
2022-12-22 23:10:13 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,15 +1,10 @@
 import axios from "axios";
-
 const user = JSON.parse(localStorage.getItem('user'));
 export default axios.create({
     baseURL: 'https://sugary-gifted-enthusiasm.glitch.me/',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-<<<<<<< HEAD
-        // 'Authorization': `Bearer ${user.token}`
-=======
         'Authorization': `Bearer ${user ? user.token : null}`
->>>>>>> d25b586a346da90c23df51300c486f5caaab4152
     }
 })
